a57c80235922994716f4002d156a00c299f499f5
BiancaBrAlmeida <biancabr.almeida@gmail.com>
2025-11-30 17:46:51 -0300

    estrutura semi-pronta

---
+++ src/components/Header.vue
@@ -0,0 +1,122 @@
+<script setup>
+import { Icon } from "@iconify/vue";
+import { ref } from "vue";
+const menuAberto = ref(false);
+const toggleMenu = () => {
+  menuAberto.value = !menuAberto.value;
+};
+</script>
+
+<template>
+  <header>
+    <img src="../assets/logo.svg" alt="logo" />
+    <section class="navegation">
+      <div class="busca">
+        <a href=""
+          ><Icon icon="basil:search-outline" width="33" height="33"
+        /></a>
+      </div>
+      <nav :class="['menu', { mostrar: menuAberto }]">
+        <ul>
+          <li><a href="">Início</a></li>
+          <li><a href="">Galeria</a></li>
+          <li><a href="">Favoritos</a></li>
+          <li>
+            <a href=""><Icon icon="bi:person-fill" width="30" height="30" /></a>
+          </li>
+        </ul>
+      </nav>
+    </section>
+    <button class="btn_menu" @click="toggleMenu">
+      <Icon icon="jam:menu" width="33" height="33" />
+    </button>
+  </header>
+</template>
+
+<style scoped lang="scss">
+header {
+  display: flex;
+  justify-content: space-between;
+  padding: 2rem;
+  box-shadow: 1px 1px 10px #a7a7a7;
+
+  ul {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 1rem;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: #262626;
+
+    &:hover {
+      color: #e1306c;
+      transition: 0.3s;
+    }
+  }
+}
+
+.navegation {
+  display: flex;
+  align-items: center;
+}
+
+.btn_menu {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  display: block;
+
+  &:hover {
+    color: #e1306c;
+    transition: 0.3s;
+  }
+}
+
+.menu {
+  display: none;
+}
+
+.mostrar {
+  display: flex;
+  width: 50%;
+  height: auto;
+  position: absolute;
+  top: 100px;
+  right: 0;
+  background-color: #f1f1f1;
+  box-shadow: 1px 1px 10px #a7a7a7;
+  border-bottom-left-radius: 10px;
+  border-top-left-radius: 10px;
+}
+
+@media (min-width: 768px) {
+  header {
+    ul {
+      display: flex;
+      flex-direction: row;
+      gap: 3rem;
+      align-items: center;
+    }
+  }
+
+  .menu {
+    display: flex;
+  }
+
+  .btn_menu {
+    display: none;
+  }
+
+  .navegation {
+    gap: 2rem;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,63 @@
+<script setup></script>
+
+<template>
+  <section class="hero">
+    <img
+      class="picture"
+      src="../assets/fotografo.png"
+      alt="Foto de alguém tirando foto"
+    />
+    <div class="texto">
+      <h1>Onde a paisagem vira <span class="destaque">inspiração</span>.</h1>
+      <p class="desc">
+        Explore paisagens, cores e luzes que inspiram novas histórias através da
+        fotografia.
+      </p>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.hero {
+  display: flex;
+  flex-direction: column;
+  padding: 1.5rem;
+}
+
+.picture {
+  padding: 2rem 4rem;
+}
+
+h1 {
+  color: #262626;
+  font-size: 60px;
+  text-align: center;
+
+  .destaque {
+    color: #e1306c;
+    font-size: 80px;
+  }
+}
+
+.desc {
+  color: #262626;
+  text-align: center;
+  font-size: 19px;
+  padding: 1rem 0;
+}
+
+@media (min-width: 768px) {
+  .hero {
+    flex-direction: row-reverse;
+    align-items: center;
+  }
+
+  .picture {
+    width: 50%;
+  }
+
+  .texto {
+    width: 50%;
+  }
+}
+</style>
